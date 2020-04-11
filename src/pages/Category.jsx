fc21c4b8b8016f4558804ff6a5b4a2b2590eb606
razvancir96 <>
2020-04-12 01:26:09 +0300

    removes filtering functionality

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Layout from '../components/Layout';
 import products from '../utils/products.json'
-import ProductListSidebar from '../components/ProductListSidebar';
 import ProductList from '../components/ProductList';
 
 class Category extends Component {
@@ -10,13 +9,7 @@ class Category extends Component {
         this.state = {
             category: {},
             items: [],
-            filteredItems: []
         }
-    }
-
-    filterProducts(lowerLimit, upperLimit) {
-        const filteredItems = this.state.items.filter((product) => product.price >= lowerLimit && product.price < upperLimit);
-        this.setState({ filteredItems });
     }
 
     componentDidMount() {
@@ -25,7 +18,6 @@ class Category extends Component {
         this.setState({
             category: products[categoryName],
             items: products[categoryName].items,
-            filteredItems: products[categoryName].items
         });
     }
 
@@ -34,10 +26,7 @@ class Category extends Component {
             <Layout>
                 <div className="container-fluid container-min-max-width">
                     <h2>{ this.state.category.name }</h2>
-                    <div className="row">
-                        <ProductListSidebar filterProducts={(low, high) => this.filterProducts(low, high)}/>
-                        <ProductList products={this.state.filteredItems} />
-                    </div>
+                    <ProductList products={this.state.items} />
                 </div>
             </Layout>
         );
